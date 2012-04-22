2a349b890461066a2bf866351687f0eef4ac2b2e
Ethan Estrada <ethan.estrada@gmail.com>
2012-04-22 01:38:19 -0700

    Continued work on vex header file.
    Vex header file now compiles without errors. Added a simple test shader in order to test header file. I will likely do the same for other languages so that I can easily run the code through a compiler to test for errors.

--- vex/shader_utils.h
+++ vex/shader_utils.h
@@ -26,7 +26,7 @@
 /* Simple function to compute the luminance of a color. */
 SHUfloat shu_luminance(SHUcolor rgbclr)
 {
-    return comp(rgbclr, 0)*0.299 + comp(rgbclr, 1)*0.587 + comp(rgbclr, 2)*0.114;
+    return rgbclr[0]*0.299 + rgbclr[1]*0.587 + rgbclr[2]*0.114;
 }
 
 /* light model BRDFs */
@@ -36,73 +36,34 @@ SHUfloat shu_specularBRDF(SHUvector nL;SHUnormal nN;SHUvector nV;SHUfloat rough)
     return specularBRDF(nL, nN, nV, rough);
 }
 
+SHUfloat shu_phongBRDF(SHUvector nL;SHUnormal nN;SHUvector nV;SHUfloat rough)
+{
+    return phongBRDF(nL, nN, nV, rough);
+}
+
+SHUfloat shu_blinnBRDF(SHUvector nL;SHUnormal nN;SHUvector nV;SHUfloat rough)
+{
+    return blinnBRDF(nL, nN, nV, rough);
+}
+
+SHUfloat shu_diffuseBRDF(SHUvector nL;SHUnormal nN;SHUvector nV;SHUfloat rough)
+{
+    return diffuseBRDF(nL, nN, nV, rough);
+}
+
+SHUfloat shu_diffuseBRDF(SHUvector nL;SHUnormal nN)
+{
+    return diffuseBRDF(nL, nN);
+}
 
 /* Specular function that accounts for light categories. */
 SHUcolor shu_specular(string cat; SHUnormal nN; SHUvector nI; SHUfloat roughness)
 {
     SHUcolor Cout = 0;
-    illuminance(cat, P, nN, M_PI/2, SPECULAR_LIGHTS)
+    illuminance(P, nN, M_PI/2, SPECULAR_LIGHTS)
     {
         Cout += Cl * SHUspecularbrdf(normalize(L), nN, nI, roughness);
     }
-    return Cout;
-}
-
-/* Phong function that accounts for light categories. */
-color shu_phong(string cat; normal N; vector V; float size)
-{
-    color Cout = 0;
-    vector R = reflect(normalize(V),normalize(N));
-        illuminance(cat, P, N, PI/2)
-        {
-            extern vector L; extern color Cl;
-            
-            float nonspec = 0;
-            lightsource("__nonspecular", nonspec);
-            if(nonspec < 1)
-            {
-                vector Ln = normalize(L);
-                Cout += (1 - nonspec) * Cl * pow(max(0.0,R.Ln),size);
-            }
-        }
-    return Cout;
-}
-
-/* Lambert function that accounts for light categories. */
-color shu_diffuse(string cat; normal N)
-{
-    extern point P;
-    color Cout = 0;
-    normal Nn = normalize(N);
-    illuminance(cat, P, N, PI/2)
-    {
-        float nondiff = 0;
-        lightsource("__nondiffuse", nondiff);
-        if(nondiff < 1)
-        {
-            extern vector L; extern color Cl;
-            vector Ln = normalize(L);
-            Cout += (1 - nondiff) * Cl * Ln.Nn;
-        }
-    }
-    return Cout;
-}
-
-/* Oren-Nayar function that accounts for light categories. */
-color shu_orennayar(string cat; normal N; vector V; float rough)
-{
-    color Cout = 0;
-
-    if(rough < 0.0001) // Account for possible rounding errors of RIB exporters.
-    {
-        /* Once the roughness nears zero, use a lambert since it is cheaper */
-        Cout = shu_diffuse(cat, N);
-    }
-    else
-    {
-        /* Not yet implemented. */
-    }
-    
     return Cout;
 }
 
